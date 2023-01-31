f9573902d70f1ee492af2c0365012e879725d092
Atanas Stankin <77876390+astankin@users.noreply.github.com>
2023-01-31 11:19:51 +0200

    Add files via upload

---
+++ Table Relations/6.University Database.sql
@@ -0,0 +1,44 @@
+USE university;
+
+CREATE TABLE subjects (
+subject_id INT PRIMARY KEY AUTO_INCREMENT,
+subject_name VARCHAR(50)
+);
+
+CREATE TABLE majors (
+major_id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50)
+);
+
+CREATE TABLE students (
+student_id INT PRIMARY KEY AUTO_INCREMENT,
+student_number VARCHAR(12),
+student_name VARCHAR(50),
+major_id INT NOT NULL,
+CONSTRAINT fk_major_student
+FOREIGN KEY (major_id)
+REFERENCES majors(major_id)
+);
+
+CREATE TABLE payments (
+payment_id INT PRIMARY KEY AUTO_INCREMENT,
+payment_date DATE,
+payment_amount DECIMAL(8,2),
+student_id INT NOT NULL,
+CONSTRAINT fk_payment_student
+FOREIGN KEY (student_id)
+REFERENCES students(student_id)
+);
+
+CREATE TABLE agenda (
+student_id INT,
+subject_id INT,
+CONSTRAINT pk
+PRIMARY KEY (student_id, subject_id),
+CONSTRAINT fk_agenda_student
+FOREIGN KEY (student_id)
+REFERENCES students(student_id),
+CONSTRAINT fk_agenda_subject
+FOREIGN KEY (subject_id)
+REFERENCES subjects(subject_id)
+);
